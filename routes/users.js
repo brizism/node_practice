7c01caab575e838f5e0d321fbad47f55e5ce95b7
brizism <montielbriseida@gmail.com>
2018-08-10 17:20:54 -0400

    login and register pages

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 // User Login Route
 router.get('/login', (req, res) => {
-  res.send('Login');
+  res.render('users/login');
 })
 
 // User Register Route
 router.get('/register', (req, res) => {
-  res.send('Register');
+  res.render('users/register');
 })
 
 module.exports = router;
