67e96dccb634a32c98e4ec1f02da739a3c935f67
brizism <montielbriseida@gmail.com>
2018-08-13 17:06:37 -0400

    set up google strategy

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://bris:holahello7@ds119422.mlab.com:19422/storybooqs-dev',
+  googleClientID: '752437166783-kk6m21pnl6i4pld45crt9230ns9256cn.apps.googleusercontent.com',
+  googleClientSecret: 'Q9-kNgGM41z0-UpC1CtCqZCB'
+}
