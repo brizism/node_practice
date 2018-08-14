ed6bf2d8091c0df407ef8c3e0a1e9dbac4a663f7
brizism <montielbriseida@gmail.com>
2018-08-13 20:40:55 -0400

    dashboard and stories routes and templates

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ require('./models/user');
 require('./config/passport')(passport);
 
 // Load Routes
-const auth = require('./routes/auth');
 const index = require('./routes/index');
+const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 // Load Keys
 const keys = require('./config/keys');
@@ -53,6 +54,7 @@ app.use((req, res, next) => {
 // Use Routes
 app.use('/', index);
 app.use('/auth', auth);
+app.use('/stories', stories);
 
 const port = process.env.PORT || 5000;
 
