8868562338495608078e7a803651b8311c001ed3
brizism <montielbriseida@gmail.com>
2018-08-20 19:00:39 -0400

    show edit icon helper func

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const {
   truncate,
   stripTags,
   formatDate,
-  select
+  select,
+  editIcon
 } = require('./helpers/hbs');
 
 // Map global promises
@@ -53,7 +54,8 @@ app.engine('handlebars', exphbs({
     truncate,
     stripTags,
     formatDate,
-    select
+    select,
+    editIcon
   },
   defaultLayout: 'main'
 }));
