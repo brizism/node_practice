e0de9526097d07b6531b9a75859fc05c5c88f8fb
brizism <montielbriseida@gmail.com>
2018-08-21 20:43:35 -0400

    update connect to useMongoClient method

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const {
 // Map global promises
 mongoose.Promise = global.Promise;
 // Mongoose Connect
-mongoose.connect(keys.mongoURI)
+mongoose.useMongoClient(keys.mongoURI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
 
